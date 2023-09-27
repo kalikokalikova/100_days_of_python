44b6bc9af577443f3a173328e6dac42436e4eac6
kalikokalikova <calico.seders@gmail.com>
2023-09-27 11:22:45 -0700

    day 8, encoding and decoding, refactor

--- 8_day.py
+++ 8_day.py
@@ -1,37 +1,24 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encode(message, shift):
-  encoded_message = ""
-  for l in message:
-    new_l_code = alphabet.index(l) + shift
-    if new_l_code > 25:
-      new_l_code = new_l_code - 26
-    encoded_message += alphabet[new_l_code]
-  return encoded_message
+def caesar(message, shift, shift_direction):
+  if shift_direction == "decode":
+    shift *= -1
+  result = ""
+  for char in message:
+    shifted_char_index = (alphabet.index(char) + shift) % 26
+    result += alphabet[shifted_char_index]
+  return result
 
-def decode(message, shift):
-  decoded_message = ""
-  for l in message:
-    new_l_code = alphabet.index(l) - shift
-    if new_l_code < 0:
-      new_l_code = new_l_code + 26
-    decoded_message += alphabet[new_l_code]
-  return decoded_message
+session_active = True
 
-command = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
-
-if command == "encode":
-  message_to_encode = input("Type your message: ")
-  # remove spaces, lower case
-  message_to_encode = list(message_to_encode.replace(" ","").lower())
-  shift_number = int(input("Type your shift number: ")) # TODO deal with 23> shift input
-  # do shift
-  encoded_message = encode(message_to_encode, shift_number)
-  print(f"Your encoded message is: {encoded_message}")
-elif command == "decode":
-  message_to_decode = input("Type your message to decode: ")
+while session_active:
+  command = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+  message = input("Type your message: ")
+  message = list(message.replace(" ","").lower())
   shift_number = int(input("Type your shift number: "))
-  decoded_message = decode(message_to_decode, shift_number)
-  print(f"Your decoded message is: {decoded_message}")
-else:
-  print("That's not a thing. Please try again.")
+  shifted_message = caesar(message, shift_number, command)
+  print(f"Your {command}d message is: {shifted_message}\n")
+  keep_going = input("Would you like to have another go? Type 'yes' or 'no': ")
+  print()
+  session_active = bool(keep_going == "yes")
+
